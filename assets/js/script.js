79431db962ac943c330cb0fb514434bc1dc45954
Ayush <sahuayush221000@gmail.com>
2023-02-01 00:40:47 +0530

    final commit: project is done CURD operation also working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,28 @@
-let designcl = ['work', 'Personal', 'Claeaning', 'Otheres'] //creating class for implementing design to different category 
+let designcl = ['Personal', 'Work', 'School', 'Cleaning', 'Other'] //creating class for implementing design to different category 
 $(document).ready(function() {
 
     let categorys = document.getElementsByClassName('catesec'); // getting all the class name category 
     for (let i = 0; i < categorys.length; i++) { // looping in the  categorys to find the which categry class belongs and implement according sesign check home.css to get the color of eact section
-        if (categorys[i].innerHTML.trim() == 'Work') {
+        if (categorys[i].innerHTML.trim() == 'Personal') {
             categorys[i].classList.add(designcl[0])
             categorys[i].classList.add('commonClass')
-        } else if (categorys[i].innerHTML.trim() == 'Personal') {
+        } else if (categorys[i].innerHTML.trim() == 'Work') {
             categorys[i].classList.add(designcl[1])
             categorys[i].classList.add('commonClass')
-        } else if (categorys[i].innerHTML.trim() == 'Claeaning') {
+        } else if (categorys[i].innerHTML.trim() == 'School') {
             categorys[i].classList.add(designcl[2])
             categorys[i].classList.add('commonClass')
-        } else if (categorys[i].innerHTML.trim() == 'Otheres') {
+        } else if (categorys[i].innerHTML.trim() == 'Cleaning') {
             categorys[i].classList.add(designcl[3])
+            categorys[i].classList.add('commonClass')
+        } else if (categorys[i].innerHTML.trim() == 'Other') {
+            categorys[i].classList.add(designcl[4])
             categorys[i].classList.add('commonClass')
         }
     }
 });
+
+
 
 // this in responsible for making  making cross line when the idem is  checked for deleting
 function checkedOrNot() {
